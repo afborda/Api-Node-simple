d95c108301e0e348f373301944c5e97eebb383d2
Abner Fonseca <abner.borda@gmail.com>
2020-03-31 22:58:18 -0300

    Ajustes

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -3,14 +3,16 @@ const users = require("../mock/MockUsers");
 module.exports = {
   show(req, res) {
     const loginData = req.body;
-
+    // console.log(loginData);
     const user = users.filter(
       item =>
         item.login === loginData.login && item.password === loginData.password
     );
 
-    if (user === []) {
-      return res.status(400).json({ error: "No Movie found " });
+    console.log("teste", user);
+
+    if (user.length < 1) {
+      return res.status(400).json({ error: "No User found " });
     }
     return res.json(user);
   }
--- src/mock/MockUsers.js
+++ src/mock/MockUsers.js
@@ -6,10 +6,60 @@ const users = [
     name: "Abner Borda Fonseca",
     password: "123456789",
     image: "N/A",
-    preferredMovies: {}
+    preferredMovies: [
+      {
+        category: "action",
+        Title: "The Incredible Hulk",
+        Year: "2008",
+        imdbID: "tt0800080",
+        Type: "movie",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BMTUyNzk3MjA1OF5BMl5BanBnXkFtZTcwMTE1Njg2MQ@@._V1_SX300.jpg"
+      },
+      {
+        category: "action",
+
+        Title: "Hulk",
+        Year: "2003",
+        imdbID: "tt0286716",
+        Type: "movie",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BMzQwZDg1MGEtN2E5My00ZDJlLWI4MzItM2U2MjJhYzlkNmEzXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_SX300.jpg"
+      },
+      {
+        category: "action",
+
+        Title: "Planet Hulk",
+        Year: "2010",
+        imdbID: "tt1483025",
+        Type: "movie",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BYmFlNTNhNjktNDQ4NC00ZmVhLTg2NmYtOWExMmI0MzQ1ODFmL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+      },
+      {
+        category: "action",
+
+        Title: "Hulk Vs.",
+        Year: "2009",
+        imdbID: "tt1325753",
+        Type: "movie",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BMTc1NzMzMzU4Nl5BMl5BanBnXkFtZTcwNTQ3NDYwNQ@@._V1_SX300.jpg"
+      },
+      {
+        category: "action",
+
+        Title: "The Incredible Hulk",
+        Year: "1977–1982",
+        imdbID: "tt0077031",
+        Type: "series",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BODc3YTIwY2ItMDU3MS00ZTFlLWJjOTAtNmE4M2M0NWI4MWE1XkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_SX300.jpg"
+      }
+    ]
   },
   {
-    id: 1,
+    id: 2,
     login: "abner2",
     email: "abner.borda2@gmail.com",
     name: "Abner Borda Fonseca",
@@ -18,7 +68,7 @@ const users = [
     preferredMovies: {}
   },
   {
-    id: 1,
+    id: 3,
     login: "abner3",
     email: "abner.borda3@gmail.com",
     name: "Abner Borda Fonseca",
