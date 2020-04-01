f55f09f2198ef912e933ee4f65a77267b01d8711
Abner Fonseca <abner.borda@gmail.com>
2020-04-01 12:47:19 -0300

    Ajustes Mock

--- src/mock/MockMovies.js
+++ src/mock/MockMovies.js
@@ -1,5 +1,6 @@
 const movies = [
   {
+    views: 3440,
     category: "action",
     Title: "The Incredible Hulk",
     Year: "2008",
@@ -9,8 +10,8 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMTUyNzk3MjA1OF5BMl5BanBnXkFtZTcwMTE1Njg2MQ@@._V1_SX300.jpg"
   },
   {
+    views: 3440,
     category: "action",
-
     Title: "Hulk",
     Year: "2003",
     imdbID: "tt0286716",
@@ -19,6 +20,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMzQwZDg1MGEtN2E5My00ZDJlLWI4MzItM2U2MjJhYzlkNmEzXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_SX300.jpg"
   },
   {
+    views: 30,
     category: "action",
 
     Title: "Planet Hulk",
@@ -29,6 +31,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BYmFlNTNhNjktNDQ4NC00ZmVhLTg2NmYtOWExMmI0MzQ1ODFmL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
   },
   {
+    views: 30,
     category: "action",
 
     Title: "Hulk Vs.",
@@ -39,6 +42,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMTc1NzMzMzU4Nl5BMl5BanBnXkFtZTcwNTQ3NDYwNQ@@._V1_SX300.jpg"
   },
   {
+    views: 4430,
     category: "action",
 
     Title: "The Incredible Hulk",
@@ -49,6 +53,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BODc3YTIwY2ItMDU3MS00ZTFlLWJjOTAtNmE4M2M0NWI4MWE1XkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_SX300.jpg"
   },
   {
+    views: 30,
     category: "action",
 
     Title: "The Incredible Hulk Returns",
@@ -59,6 +64,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMTI5NjQ0NzczN15BMl5BanBnXkFtZTcwNzM1ODMyMQ@@._V1_SX300.jpg"
   },
   {
+    views: 390,
     category: "action",
 
     Title: "The Incredible Hulk",
@@ -69,6 +75,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BYmVjYzA3MmYtZjBhOS00OGI5LWIzZTEtOTRkZGY2ZWQ5NjkzXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_SX300.jpg"
   },
   {
+    views: 39990,
     category: "action",
 
     Title: "Hulk and the Agents of S.M.A.S.H.",
@@ -79,6 +86,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMTg1MzIxMzM4Nl5BMl5BanBnXkFtZTcwMTg3Mzc5OQ@@._V1_SX300.jpg"
   },
   {
+    views: 3990,
     category: "action",
 
     Title: "The Trial of the Incredible Hulk",
@@ -89,6 +97,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BZDg3NmYwMDQtNTE5MC00NDFlLWI3YjktMWNjZDlkZWVmOWRiXkEyXkFqcGdeQXVyMTg2NjYzOA@@._V1_SX300.jpg"
   },
   {
+    views: 3990,
     category: "action",
 
     Title: "The Death of the Incredible Hulk",
@@ -99,6 +108,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMTU5OTA1OTkwNV5BMl5BanBnXkFtZTcwODM3ODkyMQ@@._V1_SX300.jpg"
   },
   {
+    views: 3990,
     category: "action",
 
     Title: "Crazy, Stupid, Love.",
@@ -109,6 +119,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMTg2MjkwMTM0NF5BMl5BanBnXkFtZTcwMzc4NDg2NQ@@._V1_SX300.jpg"
   },
   {
+    views: 39990,
     category: "love",
 
     Title:
@@ -120,6 +131,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BZWI3ZTMxNjctMjdlNS00NmUwLWFiM2YtZDUyY2I3N2MxYTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
   },
   {
+    views: 3099,
     category: "love",
 
     Title: "Love Actually",
@@ -130,6 +142,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMTY4NjQ5NDc0Nl5BMl5BanBnXkFtZTYwNjk5NDM3._V1_SX300.jpg"
   },
   {
+    views: 3099,
     category: "love",
 
     Title: "Shakespeare in Love",
@@ -140,6 +153,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BM2ZkNjM5MjEtNTBlMC00OTI5LTgyYmEtZDljMzNmNzhiNzY0XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg"
   },
   {
+    views: 3099,
     category: "love",
 
     Title: "P.S. I Love You",
@@ -150,6 +164,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BNTg2MDg4MjI5NV5BMl5BanBnXkFtZTcwMzQ0MDczMw@@._V1_SX300.jpg"
   },
   {
+    views: 3099,
     category: "love",
 
     Title: "I Love You, Man",
@@ -160,6 +175,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMTU4MjI5NTEyNV5BMl5BanBnXkFtZTcwNjQ1NTMzMg@@._V1_SX300.jpg"
   },
   {
+    views: 3099,
     category: "love",
 
     Title: "Love & Other Drugs",
@@ -170,6 +186,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMTgxOTczODEyMF5BMl5BanBnXkFtZTcwMDc0NDY4Mw@@._V1_SX300.jpg"
   },
   {
+    views: 3099,
     category: "love",
 
     Title: "Punch-Drunk Love",
@@ -180,6 +197,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BYmE1OTY4NjgtYjcwNC00NWE4LWJiNGMtZmVhYTdlMWE1YzIxXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
   },
   {
+    views: 3099,
     category: "love",
 
     Title: "In the Mood for Love",
@@ -190,6 +208,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BYTJlMmIwYjAtYWJmYy00YzE2LWE1OTYtZjIyMzg0YWUwOTg4XkEyXkFqcGdeQXVyMTA1NTM1NDI2._V1_SX300.jpg"
   },
   {
+    views: 3099,
     category: "love",
 
     Title: "Love, Rosie",
@@ -200,6 +219,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMTk0Mzg1MTU1MF5BMl5BanBnXkFtZTgwMjU3ODI2MzE@._V1_SX300.jpg"
   },
   {
+    views: 3022,
     category: "fear",
 
     Title: "Planet Terror",
@@ -210,6 +230,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BODdmNmM2ZTgtODM1MS00ZGMxLTk3MTMtMGQ1ODZhMTQ1NWEwL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
   },
   {
+    views: 3022,
     category: "fear",
 
     Title: "The Terror",
@@ -220,6 +241,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BYTJjODJjMjAtMmQzYi00OTI2LWFiMTktNDBiZjY5MDA3MWE1XkEyXkFqcGdeQXVyNjg3MDMxNzU@._V1_SX300.jpg"
   },
   {
+    views: 3022,
     category: "fear",
 
     Title: "Birdemic: Shock and Terror",
@@ -230,6 +252,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BZTMyNmM1ODYtZmU3OS00NWIwLTg0MWEtNDExY2NlMzYzNTQ4XkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_SX300.jpg"
   },
   {
+    views: 3022,
     category: "fear",
 
     Title: "Toy Story of Terror",
@@ -240,6 +263,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BYTZkY2JjMDItYzEwMS00MjVlLTlhYjktMmI5YjYzMjdmZGMxXkEyXkFqcGdeQXVyNDIwODAwNzg@._V1_SX300.jpg"
   },
   {
+    views: 3022,
     category: "fear",
 
     Title: "Snow White: A Tale of Terror",
@@ -250,6 +274,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMzE0MTk2OGYtMmM5ZC00MGQ4LTk1MGYtZTQ4Y2MzN2M4MzE4XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
   },
   {
+    views: 320,
     category: "fear",
 
     Title: "Terror Train",
@@ -260,6 +285,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BZjNkZmUwOGQtZGZjNC00MTIzLWFmYzItNmQ4ZTA0ZjJlYzM2XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
   },
   {
+    views: 3990,
     category: "fear",
 
     Title: "Zankyou No Terror",
@@ -270,6 +296,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMDMzNGEyNzItOTEyYi00MzQxLTgxOGQtZGJkZTRmNjUzN2Y3XkEyXkFqcGdeQXVyMjQ1NjEyNzE@._V1_SX300.jpg"
   },
   {
+    views: 3880,
     category: "fear",
 
     Title: "The Terror",
@@ -280,6 +307,7 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BZDIxMTIzYWEtNTgwOC00MzRmLWE1MWQtMTY3Y2ZhOWJmZGRmXkEyXkFqcGdeQXVyMTQ2MjQyNDc@._V1_SX300.jpg"
   },
   {
+    views: 380,
     category: "fear",
 
     Title: "Galaxy of Terror",
@@ -290,8 +318,8 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BNTE0MjkzMWItYTI3ZS00ZGRiLTllOTktMGMyNzlkMjRmZWZmXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
   },
   {
+    views: 3990,
     category: "fear",
-
     Title: "Tales of Terror",
     Year: "1962",
     imdbID: "tt0056552",
